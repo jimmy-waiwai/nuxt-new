ce1a525b8fdfce689330dc1495c6496f8b285fe0
jimmy16 <waiwai@bi.ciao.jp>
2026-01-18 11:58:28 +0900

    20260118_更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // SSG（静的サイト生成）を有効にする
+  ssr: true, 
+
+  // プリレンダリングの設定（必要に応じて）
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
+
+  // ★ここが重要：GitHubのリポジトリ名を設定する
+  // デプロイ先が https://username.github.io なら '/my-app/'
+  // デプロイ先が https://username.github.io なら '/'
+  app: {
+    baseURL: '/waiwai-test2/',
+    buildAssetsDir: '/_nuxt/', // assetsのパスを固定
+  },
 })
